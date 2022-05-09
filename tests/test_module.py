fbe33027b73742727434117f577fc53a2a863657
Adstefnum <bauerstevenson@gmail.com>
2022-05-09 15:01:57 +0300

    modifying

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,7 +1,7 @@
 import pytest
 from modules.module import isPowerofTwo
 
-def test_isPowerofTwo(n):
+def test_isPowerofTwo():
     assert isPowerofTwo(0) is False
     assert isPowerofTwo(1) is True
     assert isPowerofTwo(16) is True
