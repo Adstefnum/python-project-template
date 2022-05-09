b22717994a57b65a65301a94052c74b4436a6346
Adstefnum <bauerstevenson@gmail.com>
2022-05-09 14:22:00 +0300

    tests configs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,8 @@
-impoer pytest
+import pytest
+
+@pytest.fixture(scope="session")
+def db_conn():
+    db = ...
+    url = ...
+    with db.connect(url) as conn:  # connection will be torn down after all tests finish
+        yield
